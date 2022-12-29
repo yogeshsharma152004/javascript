5cf0d7d96253b2bc351452cf822a042c7ffd6af9
sharmayogesh152004@gmail.com <sharmayogesh152004@gmail.com>
2022-12-29 21:15:20 +0530

    new program added

--- math.js
+++ math.js
@@ -1,1 +1,36 @@
 // math in java script
+
+
+// Foe PI
+
+const PI = Math.PI
+console.log(PI)
+
+//for round
+
+console.log(Math.round (PI)) // it gives rounded value 
+console.log(Math.round(9.9))
+
+// floor value 
+
+console.log(Math.floor(9.5)) // it gives value down like 9.5 then it give 9
+
+// Ceil vlue
+
+console.log(Math.ceil(9.5)) // it gives value up like 9.5 then it give 10
+
+// min and max value
+
+console.log(Math.min(1,2,3,4,5,6,7,8,9)) // it print min value
+
+console.log(Math.max(1,2,3,4,5,6,7,8,9)) // it print max value
+
+// Random values
+
+console.log(Math.random())
+
+console.log(Math.random()*10) // it print any random values
+
+// power
+
+console.log(Math.pow(2,4)) // it gives power of value
