fc7739016891c702d675742f0d958e6af94b1a3b
sharmayogesh152004@gmail.com <sharmayogesh152004@gmail.com>
2023-01-07 21:30:58 +0530

    code update

---
+++ calculatorfunction.js
@@ -0,0 +1,21 @@
+/*function sum (value1,value2){
+
+
+    console.log(value1+value2,value1-value2,value1*value2,value1/value2,value1%value2);
+}
+
+sum (10,20)*/
+
+
+function calculator (){
+
+    let a = 10;
+    let b= 20;
+
+    console.log("add,sub,div,mul,module:",a+b,a-b,a/b,a*b,a%b);
+
+
+
+}
+
+calculator()
